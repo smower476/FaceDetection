071341872789fad76f4d0e80b2e4397fa40a400e
chicken <121474491+smower476@users.noreply.github.com>
2024-01-15 08:40:24 +0300

    images on cheeks

--- main.cpp
+++ main.cpp
@@ -11,34 +11,84 @@
 #include <opencv2/cudastereo.hpp>
 #include <opencv2/cudawarping.hpp>
 #include <opencv2/cudafeatures2d.hpp>
-
+#include "opencv2/imgproc.hpp"
 
 using namespace std;
 using namespace cv;
 
 //		vector<cv::Rect> faces;
-void CheeksDetection(vector<cv::Rect>& tmpRect, Mat& tmpImg) {
-	for (const auto& face : tmpRect) {
-		// Extract the region of interest (ROI) for the face
-		//Mat face_roi = gray_image(face);
+int CheeksDetection(vector<cv::Rect>& tmpRect, Mat& tmpImg) {
+	Mat overlayImage = imread("overlay_image.png", IMREAD_UNCHANGED);
+	
+	if (overlayImage.empty()) {
+		std::cerr << "Error loading overlayImage." << std::endl;
+		return -1;
+	}
+	
 
-		// Define the region of interest for the left cheek (adjust these values based on your needs)
+
+	for (const auto& face : tmpRect) {
 		int x_cheekA = face.x + face.width / 6;
 		int y_cheekA = face.y + face.height / 2;
 		int width_cheekA = face.width / 4;
 		int height_cheekA = face.height / 4;
 
-		// Define the region of interest for the right cheek (adjust these values based on your needs)
 		int x_cheekB = face.x + face.width * 4 / 6;
 		int y_cheekB = face.y + face.height / 2;
 		int width_cheekB = face.width / 4;
 		int height_cheekB = face.height / 4;
 
+		cv::Rect roiRectA(x_cheekA, y_cheekA, width_cheekA, height_cheekA);
+		cv::Rect roiRectB(x_cheekB, y_cheekB, width_cheekB, height_cheekB);
+		cv::Mat roi = tmpImg(roiRectA).clone();
+
+		Mat resized_image;
+		resize(overlayImage, resized_image, Size(width_cheekA, height_cheekA), INTER_LINEAR);
+
+		
+		//cv::medianBlur(roi, roi, 111);
+
+		if (resized_image.channels() != roi.channels()) {
+			cv::cvtColor(resized_image, resized_image, cv::COLOR_RGBA2RGB);
+		}
+
+		//cout << "ROIrows " << roi.rows << " ROIcols " << roi.cols << " channels " << roi.channels() << endl;
+		//cout << "rows " << resized_image.rows << " cols " << resized_image.cols << " channels " << resized_image.channels() << endl;
+		if (resized_image.channels() != roi.channels() || roi.cols != resized_image.cols || resized_image.channels() != roi.channels()) {
+			std::cerr << "Error: Number of channels mismatch between source and destination." << std::endl;
+			return -1;
+		}
+
+
+
+		for (int i = 0; i < roi.cols; i++)
+		{
+			for (int j = 0; j < roi.rows; j++)
+			{
+				if (!(resized_image.at<Vec3b>(j, i) == Vec3b(255, 255, 255)))
+				{
+
+					tmpImg(roiRectA).at<Vec3b>(j, i) = Vec3b(255, 41, 251);// resized_image.at<Vec3b>(j, i);
+					tmpImg(roiRectB).at<Vec3b>(j, i) = Vec3b(255, 41, 251);
+					//tmpImg(roiRect).setTo(cv::Scalar(0, 0, 255));
+
+				}
+			}
+		}
+
+
+
+	
+
+		
+
 		// Draw a rectangle around the cheeks
 		rectangle(tmpImg, Rect(x_cheekA, y_cheekA, width_cheekA, height_cheekA), Scalar(0, 255, 0), 2);
 		rectangle(tmpImg, Rect(x_cheekB, y_cheekB, width_cheekB, height_cheekB), Scalar(0, 255, 0), 2);
 	}
+	return 1;
 }
+
 
 int main(int argc, char** argv) {
 	//Gpu Info
@@ -90,6 +140,7 @@ int main(int argc, char** argv) {
 	}
 
 	cap.release();
+	_sleep(10000);
 	destroyAllWindows();
 	return 0;
 }
